9f0ba4db35c7ce25d39eb4b05c32a112bc7c1592
Kosarevic <kosarevic@gmail.com>
2020-06-22 15:03:43 +0200

    Added comments to the code.

--- Zadatak_1/Program.cs
+++ Zadatak_1/Program.cs
@@ -8,13 +8,18 @@ using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
+    /// <summary>
+    /// Application simulates example of Monitor.Pusle and Monitor.Wait.
+    /// </summary>
     class Program
     {
+        //Static variables necessary for application functionality.
         public static int[,] matrix;
         public static int[] numbers = new int[10000];
         public static readonly Random r = new Random();
         public static readonly object TheLock = new object();
         public static int[] unequalNumbers;
+        public static bool done = false;
 
         static void Main(string[] args)
         {
@@ -34,20 +39,24 @@ namespace Zadatak_1
 
             Console.ReadLine();
         }
-
+        /// <summary>
+        /// Method responsible for initiating the matrix and populating it with random numbers.
+        /// </summary>
         public static void GenerateMatrix()
         {
+            //Matrix is initialized.
             matrix = new int[100, 100];
 
             lock (TheLock)
             {
-                while (numbers.Count() != 10000)
+                //Untill Thread t2 finishes generating values, thread pauses here.
+                if (numbers.Count() != 10000)
                 {
                     Monitor.Wait(TheLock);
                 }
 
                 int count = 0;
-
+                //Random numbers are being assigned to matrix in code below.
                 for (int i = 0; i < matrix.GetLength(0); i++)
                 {
                     for (int j = 0; j < matrix.GetLength(1); j++)
@@ -58,7 +67,9 @@ namespace Zadatak_1
                 }
             }
         }
-
+        /// <summary>
+        /// Method responsible for generating 10000 random numbers ment for populating the matrix.
+        /// </summary>
         public static void GenerateNumbers()
         {
             lock (TheLock)
@@ -67,15 +78,17 @@ namespace Zadatak_1
                 {
                     numbers[i] = r.Next(10, 99);
                 }
-
+                
                 Monitor.Pulse(TheLock);
             }
         }
-
+        /// <summary>
+        /// Method finds all unequal numbers present in 10000 number list and stores them in a file.
+        /// </summary>
         public static void UnequalNumbers()
         {
             List<int> temp = new List<int>();
-
+            //Search for unequal numbers.
             foreach (int i in matrix)
             {
                 if (i % 2 != 0)
@@ -95,6 +108,7 @@ namespace Zadatak_1
 
             lock (TheLock)
             {
+                //Unequal numbers are being written in txt file.
                 using (StreamWriter sw = new StreamWriter("..//..//Files/UnequalNumbers.txt"))
                 {
                     foreach (int i in unequalNumbers)
@@ -103,19 +117,22 @@ namespace Zadatak_1
                     }
                 }
 
+                done = true;
                 Monitor.Pulse(TheLock);
             }
         }
-
+        /// <summary>
+        /// Method displays all unequal numbers written on a text file.
+        /// </summary>
         public static void DisplayUnequalNumbers()
         {
             lock (TheLock)
             {
-                while (numbers.Count() != 10000)
+                if (!done)
                 {
                     Monitor.Wait(TheLock);
                 }
-
+                //Unequal numbers are being displayed on console.
                 string line = "";
                 using (StreamReader sr = new StreamReader("..//..//Files/UnequalNumbers.txt"))
                 {
@@ -123,7 +140,7 @@ namespace Zadatak_1
                     {
                         Console.WriteLine(line);
                     }
-                } 
+                }
             }
         }
     }
