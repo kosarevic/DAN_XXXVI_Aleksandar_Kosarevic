d01b64393edc35905d5835e8236374db797092b6
Kosarevic <kosarevic@gmail.com>
2020-06-22 13:02:50 +0200

    Created GenerateNumbers() method

--- Zadatak_1/Program.cs
+++ Zadatak_1/Program.cs
@@ -9,22 +9,63 @@ namespace Zadatak_1
 {
     class Program
     {
+        public static int[,] matrix;
+        public static int[] numbers = new int[10000];
+        public static readonly object TheLock = new object();
+
         static void Main(string[] args)
         {
-            Thread t = new Thread(GenerateMatrix);
-            t.Start();
-            
+            Thread t1 = new Thread(GenerateMatrix);
+            Thread t2 = new Thread(GenerateNumbers);
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+
+            foreach (var item in matrix)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
         }
 
         public static void GenerateMatrix()
         {
-            int[,] matrix = new int[100, 100];
+            matrix = new int[100, 100];
+
+            lock (TheLock)
+            {
+                while (numbers.Count() != 10000)
+                {
+                    Monitor.Wait(TheLock);
+                }
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        foreach (int k in numbers)
+                        {
+                            matrix[i, j] = k;
+                        }
+                    }
+                } 
+            }
         }
 
         public static void GenerateNumbers()
         {
             Random r = new Random();
 
+            lock (TheLock)
+            {
+                for (int i = 0; i < 10000; i++)
+                {
+                    numbers[i] = r.Next(10, 99);
+                }
+
+                Monitor.Pulse(TheLock);
+            }
         }
     }
 }
